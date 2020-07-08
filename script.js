6e8152069c3da8f23622785b6266f621cf219d59
Qua Nguyen <nguyenmqua@gmail.com>
2020-07-08 17:00:15 -0600

    made the various prompt randomize and console.log

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //array for password
 
 var lettersArray = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"];
-var captialArray =  ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"]
+var capitalArray =  ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"];
+var numberArray =  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",];
+var passwordArray = []
 
 
 
@@ -11,22 +13,39 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var letterCount = prompt("How many character do you want (8-128)?");
-  console.log (letterCount);
-  var capitalLetter = confirm("Click okay to confirm if you want capital letters in your password");
-  console.log (capitalLetter)
-  var specialCharacter = confirm("Click okay to confirm if you want specialized letters in your password");
-  console.log (specialCharacter)
-  var numberCharacter = confirm("Click okay to confirm if you want numbers in your password");
-  console.log(numberCharacter)
+  var confirmCount = prompt("How many character do you want (8-128)?");
+  console.log (confirmCount);
 
+  var confirmCapital = confirm("Click okay to confirm if you want CAPITAL letters in your password");
+  console.log (confirmCapital);
+  if (confirmCapital) {
+    var randomCapital = capitalArray[Math.floor(Math.random() * capitalArray.length)];
+  console.log (confirmCapital)
+  passwordArray.push(randomCapital)
+  console.log (passwordArray)}
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
+  var confirmLetter = confirm("Click okay to confirm if you want lower-case letters in your password");
+  console.log (confirmLetter);
+  if (confirmLetter) {
+    var randomLetter = lettersArray[Math.floor(Math.random() * lettersArray.length)];
+  console.log (randomLetter)
+  passwordArray.push(randomLetter)
+  console.log (passwordArray)}
+
+  var confirmCharacter = confirm("Click okay to confirm if you want specialized letters in your password");
+  console.log (confirmCharacter);
+
+  var confirmNumber = confirm("Click okay to confirm if you want numbers in your password");
+  console.log(confirmNumber);
+  if (confirmNumber) {
+    var randomNumber = numberArray[Math.floor(Math.random() * numberArray.length)];
+  console.log (randomNumber)
+  passwordArray.push(randomNumber)
+  console.log (passwordArray)}
+
 
 }
+
 
 // Add event listener to generate button
 
